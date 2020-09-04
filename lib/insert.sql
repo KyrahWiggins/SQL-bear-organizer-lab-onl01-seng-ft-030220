208fcad6aa79e424eb58807e1ed62a2d626f165d
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-09-04 17:02:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,24 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 15, 'M', 'brown', 'calm', 1);
- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 12, 'M', 'dark brown', 'rowdy', 1);
- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 11, 'F', 'brown', 'calm', 1);
- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'M', 'brown', 'calm', 1);
- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'brown', 'calm', 1);
- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'M', 'brown', 'grumpy', 1);
- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'F', 'brown', 'calm', 0);
- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 99, 'F', 'brown', 'deadly', 0); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Mr. Chocolate', 10, 'M', 'brown', 'friendly', 1);
+
+  INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Rowdy', 5, 'F', 'black', 'sassy', 1);
+
+  INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Tabitha', 8, 'F', 'white', 'diva', 1);
+
+  INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Sergeant Brown', 15, 'M', 'brown', 'goofy', 1);
+
+  INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Melissa', 2, 'F', 'pink', 'sweet', 1);
+
+  INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Grinch', 5, 'M', 'green', 'mean', 1);
+
+  INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ ('Wendy', 1, 'F', 'red', 'happy', 1);
+
+  INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+ (NULL, 20, 'M', 'brown', 'angry', 0 ); 
+
